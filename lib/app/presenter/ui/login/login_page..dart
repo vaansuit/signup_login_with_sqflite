e6f8e306e40648f4f6372db6f8ffb29c50d0e016
Victor Hugo Vansuit <vvansuit@outlook.com>
2022-08-04 16:33:20 -0300

    [feat] added login function

--- lib/app/presenter/ui/login/login_page..dart
+++ lib/app/presenter/ui/login/login_page..dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:login_and_signup_with_sqflite/app/presenter/ui/home/home_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../../domain/entities/user_model.dart';
+import '../../../infra/datasource/local/database_helper.dart';
+import '../../logic/alert_dialog.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -8,12 +14,58 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  //preferencias de usuário
+  final Future<SharedPreferences> _pref = SharedPreferences.getInstance();
+  //form key
+  final _formKey = GlobalKey<FormState>();
+  //controllers para pegar o email e a senha
+  final emailController = TextEditingController();
+  final passController = TextEditingController();
+  //variável da base de dados
+  var dbHelper;
+
+  //função que realiza a verificação do email e da senha no banco de dados
+  login() async {
+    FocusManager.instance.primaryFocus?.unfocus;
+    String email = emailController.text;
+    String pass = passController.text;
+
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+
+      await dbHelper.getLoginUser(email, pass).than((userData) {
+        if (userData != null) {
+          setSP(userData).whenComplete(() {
+            Navigator.pushAndRemoveUntil(
+                context,
+                MaterialPageRoute(builder: (_) => HomePage(userData)),
+                (Route<dynamic> route) => false);
+          });
+        } else {
+          alertDialog(context, "Erro, usuário não encontrado!");
+        }
+      }).catchError((error) {
+        alertDialog(context, "Erro ao fazer login!");
+      });
+    }
+  }
+
+  Future setSP(UserModel user) async {
+    final SharedPreferences sp = await _pref;
+    sp.setString("userName", user.userName);
+    sp.setString("email", user.email);
+    sp.setString("password", user.password);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    dbHelper = DatabaseHelper();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(''),
-      ),
       body: Container(),
     );
   }
